e5286ce8e1613f5f8d1b9096b45898e5b2f5056d
Brett Henderson <bretth18@gmail.com>
2026-09-10 16:37:49 -0700

    filters for image filetypes in search (#102)
    * add artwork filter
    
    * use file types
    
    * bump version

--- seeleseek/Features/Search/Views/SearchFilterBar.swift
+++ seeleseek/Features/Search/Views/SearchFilterBar.swift
@@ -43,6 +43,9 @@ struct SearchFilterBar: View {
             FilterChip(label: "Hi-Res", isActive: searchState.isPresetActive(.hiRes)) {
                 searchState.applyPreset(.hiRes)
             }
+            FilterChip(label: "Artwork", isActive: searchState.isPresetActive(.artwork)) {
+                searchState.applyPreset(.artwork)
+            }
 
             Spacer()
 
@@ -90,6 +93,15 @@ struct SearchFilterPanel: View {
                 ForEach(formats, id: \.self) { ext in
                     FilterChip(label: ext.uppercased(), dimension: "Format", isActive: searchState.filterExtensions.contains(ext)) {
                         searchState.toggleExtension(ext)
+                    }
+                }
+            }
+
+            filterRow("Image") {
+                ForEach(FileTypes.imageFormats, id: \.self) { spellings in
+                    let exts = Set(spellings)
+                    FilterChip(label: spellings[0].uppercased(), dimension: "Image", isActive: exts.isSubset(of: searchState.filterExtensions)) {
+                        searchState.toggleExtensions(exts)
                     }
                 }
             }
